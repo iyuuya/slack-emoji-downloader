93e64b414546e1c830d5233077056a71bad51f5f
iyuuya <vim.ito@1pac.jp>
2017-10-03 10:54:57 +0900

    multiple team emoji downloads

--- main.rb
+++ main.rb
@@ -7,43 +7,49 @@ require 'slack'
 require 'fileutils'
 require 'open-uri'
 
-token = if ENV['TOKEN']
-          ENV['TOKEN']
-        else
-          print 'Token: '
-          gets.strip
+def bulk_download(token)
+  client = Slack::Client.new(token: token)
+
+  unless client.auth_test['ok']
+    puts "invalid-token: #{token}"
+    exit
+  end
+
+  domain = client.team_info['team']['domain']
+
+  download_dir = "emoji/#{domain}"
+  FileUtils.mkdir_p(download_dir) unless Dir.exist?(download_dir)
+  Dir.chdir(download_dir) do
+    client.emoji_list['emoji'].each do |emoji, url|
+      if url =~ /^alias:/
+        puts "skip-alias: #{emoji}"
+        next
+      end
+
+      emoji_filename = emoji + File.extname(url)
+
+      if File.exist?(emoji_filename)
+        puts "skip-file-exist: #{emoji_filename}"
+        next
+      end
+
+      puts "download: #{emoji_filename}"
+      open(emoji_filename, 'wb') do |emoji_file|
+        open(url) do |raw|
+          emoji_file.write(raw.read)
         end
-
-client = Slack::Client.new(token: token)
-
-unless client.auth_test['ok']
-  puts 'invalid token'
-  exit
-end
-
-domain = client.team_info['team']['domain']
-
-download_dir = "emoji/#{domain}"
-FileUtils.mkdir_p(download_dir) unless Dir.exist?(download_dir)
-Dir.chdir(download_dir) do
-  client.emoji_list['emoji'].each do |emoji, url|
-    if url =~ /^alias:/
-      puts "skip-alias: #{emoji}"
-      next
-    end
-
-    emoji_filename = emoji + File.extname(url)
-
-    if File.exist?(emoji_filename)
-      puts "skip-file-exist: #{emoji_filename}"
-      next
-    end
-
-    puts "download: #{emoji_filename}"
-    open(emoji_filename, 'wb') do |emoji_file|
-      open(url) do |raw|
-        emoji_file.write(raw.read)
       end
     end
   end
+end
+
+tokens = if ENV['TOKENS']
+           ENV['TOKENS'].split(',')
+         else
+           print 'Tokens(separate comma): '
+           gets.strip.split(',')
+         end
+
+tokens.each do |token|
+  bulk_download(token)
 end
